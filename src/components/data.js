cee23ef674d1e0dae2c02ba5b338ec44dcd657ec
krshmt <tourekris16@gmail.com>
2024-11-04 14:59:20 +0100

    fin

---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    title: "ASTRO LOOSE BAGGY JEANS - 30/32",
+    src: "/images/11.jpg"
+  },
+  {
+    title: "ASTRO LOOSE BAGGY JEANS - 30/32",
+    src: "/images/12.jpg"
+  },
+  {
+    title: "Furious black PreWorkout - Cola",
+    src: "/images/13.jpg"
+  },
+  {
+    title: "Arrival T-shirt - Black",
+    src: "/images/14.jpg"
+  },
+  {
+    title: "Whey Native - Vanille",
+    src: "/images/15.jpg"
+  }
+]
